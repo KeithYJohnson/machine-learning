99bb3f54ec4c620c5bf08ea15d89f28dcc3be7b1
Keith Johnson <keithyjohnson@gmail.com>
2016-10-28 13:07:02 -0400

    plotting the training data

--- machine-learning-ex2/ex2/plotData.m
+++ machine-learning-ex2/ex2/plotData.m
@@ -1,5 +1,5 @@
 function plotData(X, y)
-%PLOTDATA Plots the data points X and y into a new figure 
+%PLOTDATA Plots the data points X and y into a new figure
 %   PLOTDATA(x,y) plots the data points with + for the positive examples
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
@@ -13,8 +13,10 @@ figure; hold on;
 %
 
 
+pos = find(y == 1);
+neg = find(y == 0);
 
-
+% Plot Examples% Marker types here% https://www.mathworks.com/help/matlab/ref/linespec.html#bs3b5y3-1plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 3, ...     'MarkerSize', 7);% Colors can be found on this page% https://www.mathworks.com/help/matlab/ref/plot.htmlplot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'm', ...     'MarkerSize', 7);
 
 
 
