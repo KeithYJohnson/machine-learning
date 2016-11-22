e4c09eb25f126babc0572586c1a2d8c57504c28f
Keith Johnson <keithyjohnson@gmail.com>
2016-11-22 10:31:28 -0600

    completing ex8: Estimate Gaussian Parameters

--- machine-learning-ex8/ex8/estimateGaussian.m
+++ machine-learning-ex8/ex8/estimateGaussian.m
@@ -1,18 +1,22 @@
 function [mu sigma2] = estimateGaussian(X)
-%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%ESTIMATEGAUSSIAN This function estimates the parameters of a
 %Gaussian distribution using the data in X
-%   [mu sigma2] = estimateGaussian(X), 
+%   [mu sigma2] = estimateGaussian(X),
 %   The input X is the dataset with each n-dimensional data point in one row
 %   The output is an n-dimensional vector mu, the mean of the data set
 %   and the variances sigma^2, an n x 1 vector
-% 
+%
 
 % Useful variables
 [m, n] = size(X);
-
 % You should return these values correctly
-mu = zeros(n, 1);
-sigma2 = zeros(n, 1);
+
+mu = mean(X)'; % mean
+
+% passing 1 as second arg so that it uses n
+% instead of n-1 as for the product with
+% the summation.
+sigma2 = var(X,1)'; % variance
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the mean of the data and the variances
